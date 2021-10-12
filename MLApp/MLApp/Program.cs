2a0aef77e37b1a297f9333b2fa7ae52dccad9c02
Антон Бодров <antonybodrov@mail.ru>
2021-10-12 23:02:50 +0300

    lab1 finished

---
+++ MLApp/MLApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ClassLib;
+using System.Threading.Tasks;
+
+namespace bodrov_app
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Type image folder name");
+            string ImageFolder;
+            ImageFolder = Console.ReadLine();
+
+            var PicPro = new PicProcessing();
+            await foreach (var im in PicPro.ObjectDetecting(ImageFolder))
+            {
+                Console.WriteLine(im);
+            }
+
+
+        }
+    }
+}
